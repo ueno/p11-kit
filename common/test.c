deb3256e1fc7cce005c378d0733adc68960ac545
Daiki Ueno <dueno@redhat.com>
2020-01-03 13:05:13 +0100

    test: Include <dirent.h> and <unistd.h> only on Unix

--- common/test.c
+++ common/test.c
@@ -42,18 +42,18 @@
 #include "path.h"
 
 #include <assert.h>
-#include <dirent.h>
 #include <errno.h>
 #include <setjmp.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 
 #ifdef OS_UNIX
+#include <dirent.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
+#include <unistd.h>
 #endif
 
 enum {
