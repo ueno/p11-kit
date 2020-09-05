688f7348586269edd823d1facea8a636ee1a6e8e
Daiki Ueno <ueno@gnu.org>
2020-09-05 18:35:12 +0200

    test

--- .travis/osx/before_install.sh
+++ .travis/osx/before_install.sh
@@ -1,5 +1,6 @@
 brew update
-brew install libffi bash-completion
+brew install libffi bash-completion bison
 
 export PATH=${PATH}:/usr/local/opt/gettext/bin
+export PATH=/usr/local/opt/bison/bin:${PATH}
 export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:/usr/local/opt/libffi/lib/pkgconfig
--- .travis/osx/script.sh
+++ .travis/osx/script.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+bison -V
 NOCONFIGURE=1 ./autogen.sh
 ./configure --prefix=/installdir --libdir=/installdir/lib --sysconfdir=/installdir/etc --with-trust-paths=/installdir/etc/pki/ca-trust-source:/installdir/share/pki/ca-trust-source
 make V=1
